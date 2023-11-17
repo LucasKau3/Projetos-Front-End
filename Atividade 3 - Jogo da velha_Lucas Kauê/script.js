f74cd2cc8ee4a2b0b9aee7caa350ae094aee7dfa
Lucas Kauê <89214974+LucasKau3@users.noreply.github.com>
2023-11-16 22:34:39 -0300

    Add files via upload
    Jogo da velha web.
    Tecnologias utilizadas: HTML, CSS, JavaScript.

---
+++ Atividade 3 - Jogo da velha_Lucas Kauê/script.js
@@ -0,0 +1,70 @@
+const currentPlayer = document.querySelector(".currentPlayer");
+
+let selected;
+let player = "X";
+
+
+// Posições para se obter um vencedor.
+let positions = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
+
+function init() {
+  selected = [];
+
+  // Evento para mostrar o jogador Atual
+  currentPlayer.innerHTML = `JOGADOR ATUAL: ${player}`;
+
+  document.querySelectorAll(".game button").forEach((item) => {
+    item.innerHTML = "";
+    item.addEventListener("click", newMove);
+  });
+}
+
+init();
+
+function newMove(e) {
+  const index = e.target.getAttribute("data-i");
+  e.target.innerHTML = player;
+  e.target.removeEventListener("click", newMove);
+  selected[index] = player;
+
+  setTimeout(() => {
+    check();
+  }, [100]);
+
+  player = player === "X" ? "O" : "X";
+  currentPlayer.innerHTML = `JOGADOR DA VEZ: ${player}`;
+}
+
+function check() {
+  let playerLastMove = player === "X" ? "O" : "X";
+
+  const items = selected
+    .map((item, i) => [item, i])
+    .filter((item) => item[0] === playerLastMove)
+    .map((item) => item[1]);
+
+    
+    // Loop para verificar se há empate de acordo com as 9 posições.
+  for (pos of positions) {
+    if (pos.every((item) => items.includes(item))) {
+      alert("O JOGADOR '" + playerLastMove + "' GANHOU, PARABENS!");
+      init();
+      return;
+    }
+  }
+
+  if (selected.filter((item) => item).length === 9) {
+    alert("O JOGO EMPATOU!");
+    init();
+    return;
+  }
+}
